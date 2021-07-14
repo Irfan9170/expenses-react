fdebd3e1c3f43a3a27e7144841beac46d7f34064
Irfan <irfanali987654@gmail.com>
2021-07-14 23:58:15 +0530

    use state

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import ExpenseItem from './components/ExpenseItem';
 import React ,{useState} from 'react';
 function App() {
   const date = new Date();
+   const [name,setName]= useState('');
+   const onChange = (e)=>{
+     
+     setName(e.target.value)
+
+   }
+      const onSubmitHandler = (e)=>{
+    e.preventDefault();
+    console.log(name)
+    setName('')
+      }
  
-  const [state, setstate] = useState("irfan");
-  const clickHandeler=()=>{
-    setstate("Updatedddddddd")
-  }
   return (
     <>
       <h1>Lets Get Started </h1>
-      <h1>Hello {state}</h1> 
-      <button onClick={clickHandeler}>Click To change name</button>
+      <h1>Hello</h1>
+      <form onSubmit={onSubmitHandler}>
+        <input value={name} onChange={onChange} />
+        <button>Submit</button>
+      </form>
       <ExpenseItem date={date} title="Marketing" price="7600"/>
       <ExpenseItem date={date} title="Car Insurance" price="3600"/>
       <ExpenseItem date={date} title="Online Shopping" price="3500"/>
